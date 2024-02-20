a2b6356de237f22ff4dfda930a68046397d9ce72
hemmati-mh <hemmati-mh@agri-bank.com>
2024-02-20 13:44:28 +0330

    adding repository pattern.

--- lib/features/feature_weather/domain/entities/current_city_entity.dart
+++ lib/features/feature_weather/domain/entities/current_city_entity.dart
@@ -35,6 +35,19 @@ class CurrentCityEntity extends Equatable{
 
   @override
   List<Object?> get props => [
-
+    coord,
+    weather,
+    base,
+    main,
+    visibility,
+    wind,
+    rain,
+    clouds,
+    dt,
+    sys,
+    timezone,
+    id,
+    name,
+    cod
   ];
 }
