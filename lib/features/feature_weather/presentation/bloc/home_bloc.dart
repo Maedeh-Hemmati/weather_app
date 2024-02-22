4b3020aaf6ac93eab216e574cf74d9a27576990e
Maede Hemmati <m_hemmati@keywa.ca>
2024-02-22 14:57:52 +0330

    implement bloc state.

---
+++ lib/features/feature_weather/presentation/bloc/home_bloc.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:weather_app/core/resources/data_state.dart';
+import 'package:weather_app/features/feature_weather/domain/use_cases/get_current_weather_usecase.dart';
+import 'package:weather_app/features/feature_weather/presentation/bloc/cw_status.dart';
+
+part 'home_event.dart';
+
+part 'home_state.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  final GetCurrentWeatherUseCase getCurrentWeatherUseCase;
+
+  HomeBloc(this.getCurrentWeatherUseCase)
+      : super(HomeState(cwStatus: CwLoading())) {
+    on<LoadCwEvent>((event, emit) async {
+      emit(state.copyWith(CwLoading()));
+      DataState dataState = await getCurrentWeatherUseCase(event.cityName);
+      if (dataState is DataSuccess) {
+        emit(state.copyWith(CwCompleted(dataState.data)));
+      }
+      if (dataState is DataFailed) {
+        emit(state.copyWith(CwError(dataState.error.toString())));
+      }
+    });
+  }
+}
